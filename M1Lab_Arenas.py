82c9c3906dd9358f444ba230434e5d53b61f7d33
arenassm0340 <36685301+arenassm0340@users.noreply.github.com>
2019-01-23 12:20:06 -0500

    Add files via upload

---
+++ M1Lab_Arenas.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+CSC 221
+M1Lab
+Maria Arenas
+01/23/19
+
+"""
+def main ():
+    """ Bottles of beer song """
+    
+    for beer in range (99, -1, -1):
+        print (beer, "beers on the wall")
+        print("Take one down pass it around")
+        
+        if beer == 1: 
+            print("no more beers left.")
+        
+        
+        
+    
+        
+        
+main()
