52c57f9647301f5185f741f2cd6c32e62471c4f8
arenassm0340 <36685301+arenassm0340@users.noreply.github.com>
2019-01-28 12:44:51 -0500

    Add files via upload

---
+++ BeerSong.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+CSC 221
+beer song code
+Maria Arenas
+01/28/19
+
+@author: arenassm0340
+"""
+
+word = "bottles"
+for beer_num in range(99, 0, -1):
+    print(beer_num, word, "of beer on the wall.")
+    print(beer_num, word, "of beer.")
+    print("Take one down.")
+    print("Pass it around.")
+    if beer_num == 1:
+        print("No more bottles of beer on the wall.")
+    else:
+        new_num = beer_num - 1
+        if new_num == 1:
+            word = "bottle"
+        print(new_num, word, "of beer on the wall.")
+    print()
